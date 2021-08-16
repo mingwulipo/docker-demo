bbfdcc7901e370527ab434c03fa59128528f65ba
lipo <lipo@qq.com>
2021-08-16 08:59:32 +0800

    init

---
+++ src/main/java/org/study/docker/DockerDemoApplication.java
@@ -0,0 +1,11 @@
+package org.study.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/org/study/docker/UserController.java
@@ -0,0 +1,29 @@
+package org.study.docker;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/**
+ * @desc: TODO
+ * @author: lipo
+ * @date: 2019-09-20 18:38
+ * @version: v1.0
+ */
+@Slf4j
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    /**
+     * http://localhost:8080/user/list
+     * @return
+     */
+    @RequestMapping("list")
+    public Object listUser() {
+        return Arrays.asList(new User(1,"Jack"),new User(2,"Bill"));
+    }
+
+}
