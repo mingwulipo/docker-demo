6d10069d93658f8503c542f5e5dc47ec4b3e15cb
lipo <lipo@qq.com>
2021-08-17 15:41:02 +0800

    命令

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+#install报错，也会往下走，注意看install成功否
 mvn clean install -Dmaven.test.skip=true
 
-case $1 in
-    docker-demo)
-        echo "开始构建docker-demo项目，请记得拉取最新代码及编译通过"
-        docker build -t docker-demo:latest ./
-        docker rm -v -f docker-demo && docker run -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro --name docker-demo -d docker-demo:latest
-	;;
-    *)
-        echo "未知模块，请输入正确的参数"
-esac
+tag=`head -1 version`
+echo "tag:$tag"
+img="uccreg.iblackvip.com:5000/java/docker-demo:$tag"
+echo "image:$img"
+
+docker build -t $img ./
+docker rm -v -f docker-demo && docker run -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro --name docker-demo -d $img
+echo "创建镜像和启动容器成功!"
+
+#找冯斌创建账户和密码
+docker login https://harbor.fastai.top/
+
+docker push $img
+echo "推送镜像成功!"
