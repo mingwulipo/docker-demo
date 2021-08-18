0bf51b37aaab4c3d0c6acdf9eb43b7f701b85700
lipo <lipo@qq.com>
2021-08-18 09:18:28 +0800

    pull

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 1. git拉取最新代码，切换分支
 
 #找冯斌创建账户和密码
-2. docker login https://harbor.fastai.top/
+2. docker login http://harbor.fastai.top
 
 3. 切换到本文件所处目录
 
--- run.sh
+++ run.sh
@@ -12,5 +12,8 @@ docker build -t $img ./
 docker rm -v -f docker-demo && docker run -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro --name docker-demo -d $img
 echo "创建镜像和启动容器成功!"
 
+#push多次，tag相同的话，最新的才有tag，旧的没有tag
 docker push $img
+#其他机器拉取镜像,docker pull $img，必须加tag，否则默认tag是latest，但是没有这个tag
+
 echo "推送镜像成功!"
